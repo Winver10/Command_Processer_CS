d1e8b593cea2e4f0870f4722b5fdbd84febaf857
Smile Cai <cty11072011@outlook.com>
2026-01-20 18:19:18 +0800

    fix

--- Program/Program.cs
+++ Program/Program.cs
@@ -25,7 +25,7 @@ namespace MainProgram
             {
                 Console.Write(">");
                 List<string> commandAfterExplainde = cmexp.ExplainCommand(Console.ReadLine());
-                Command cmd = new Command(cmexp.GetID(), commandAfterExplainde)
+                Command cmd = new Command(cmexp.GetID(), commandAfterExplainde);
                 cmds.Add(cmd);
                 Dislay(cmds);
             }
