fa78593f5558acb223d4d851fad58a4f304aaf12
Winver10 <cty11072011@outlook.com>
2026-02-01 19:12:36 +0800

    Fix some bugs: logic of Backspace

--- Program/Light.cs
+++ Program/Light.cs
@@ -4,24 +4,51 @@ using System.Text;
 
 namespace LightInput
 {
+    class Memories
+    {
+        private bool _isSpace;
+        private bool _isLink;
+        private bool _inQuete;
+        public Memories(bool isSpace, bool isLink, bool inQuete)
+        {
+            _isSpace = isSpace;
+            _isLink = isLink;
+            _inQuete = inQuete;
+        }
+        public bool get_isSpace() { return _isSpace; }
+        public bool get_isLink() { return _isLink; }
+        public bool get_inQuete() { return _inQuete; }
+    }
     class ColorDrawer
     {
         private bool _isSpace = false;
         private bool _isLink = false;
         private bool _inQuete = false;
+        private int _conter = -2;
+        private List<Memories> DateSaver {get; set;} = new List<Memories>();
         public ConsoleColor GetColorForChar(char c)
         {
-            if (c == '"' && !_inQuete) { _inQuete = true; return ConsoleColor.Blue; }
-            if (c == '"' && _inQuete) { _inQuete = false; return ConsoleColor.Blue; }
-            if (c == ' ' && !_isSpace) { _isSpace = true; _isLink = false; return ConsoleColor.Gray; }
-            if (c == '-') { _isLink = true; _isSpace = false ; return ConsoleColor.Gray; }
+            _conter++;
+            if (c == '"' && !_inQuete) { _inQuete = true;DateSaver.Add(new Memories(_isSpace, _isLink, _inQuete)); return ConsoleColor.Blue; }
+            if (c == '"' && _inQuete) { _inQuete = false;DateSaver.Add(new Memories(_isSpace, _isLink, _inQuete)); return ConsoleColor.Blue; }
+            if (c == ' ' && !_isSpace) { _isSpace = true; _isLink = false;DateSaver.Add(new Memories(_isSpace, _isLink, _inQuete)); return ConsoleColor.Gray; }
+            if (c == '-' && !_inQuete) { _isLink = true; _isSpace = false ;DateSaver.Add(new Memories(_isSpace, _isLink, _inQuete)); return ConsoleColor.Gray; }
 
             if (c != ' ') { _isSpace = false; }
             
-                 if (_isLink) return ConsoleColor.Gray;
-            else if (_inQuete) return ConsoleColor.Blue;
-            else return ConsoleColor.White;
+                 if (_inQuete) {DateSaver.Add(new Memories(_isSpace, _isLink, _inQuete));return ConsoleColor.Blue;}
+            else if (_isLink){DateSaver.Add(new Memories(_isSpace, _isLink, _inQuete)); return ConsoleColor.Gray;}
+            else {DateSaver.Add(new Memories(_isSpace, _isLink, _inQuete)); return ConsoleColor.White;}
         }
+        public void ResetState()
+        {
+            _isSpace = DateSaver[_conter].get_isSpace();
+            _isLink = DateSaver[_conter].get_isLink();
+            _inQuete = DateSaver[_conter].get_inQuete();
+            _conter--;
+            DateSaver.RemoveAt(DateSaver.Count - 1);
+        }
+        
     }
     public static class Input
     {
@@ -39,7 +66,9 @@ namespace LightInput
                 {
                     input.Length--;
                     Console.Write("\b \b");
+                    color.ResetState();
                     continue;
+                    
                 }
 
                 if (!char.IsControl(keyinfo.KeyChar))
